922fc0aa977cbd021f96635e1d8391603bb48ba2
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2013-12-04 15:18:40 +0100

    Removed Misc dependence, renamed Features to MFCCs

--- fea/Feacalc.jl
+++ fea/Feacalc.jl
@@ -1,14 +1,31 @@
+## Feacalc.jl Full implementation of feature calculation for speech files. 
+## (c) 2013 David A. van Leeuwen
+
+## This program is free software: you can redistribute it and/or modify
+##     it under the terms of the GNU General Public License as published by
+##     the Free Software Foundation, version 3 of the License.
+
+##     This program is distributed in the hope that it will be useful,
+##     but WITHOUT ANY WARRANTY; without even the implied warranty of
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     GNU General Public License for more details.
+
+##     You should have received a copy of the GNU General Public License
+##     along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
 module Feacalc
 
 ## Feacalc.  Feature calculation as used for speaker and language recognition. 
 
 export feacalc, sad
 
-using Features
+using MFCCs
 using SignalProcessing
 using WAV
 using Rasta
-using Misc
+
+nrow(x) = size(x,1)
+ncol(x) = size(x,2)
 
 ## if test==true, compute features with parmeters as we use in speaker recogntion
 function feacalc(wavfile::String; augtype=:ddelta, normtype=:warp, sadtype=:energy, defaults=:spkid_toolkit, dynrange::Real=30., nwarp::Int=399)
